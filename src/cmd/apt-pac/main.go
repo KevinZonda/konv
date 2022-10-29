352c25ad6f1b1ec0c9f877a82cb2a01b3102728b
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-10-29 20:47:14 +0100

    feat: finish base

--- src/cmd/apt-pac/main.go
+++ src/cmd/apt-pac/main.go
@@ -1,36 +1,47 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/KevinZonda/apt-pac/pkg/loader"
+	"github.com/KevinZonda/apt-pac/pkg/utils"
 	"os"
 	"os/exec"
 )
 
 func main() {
-	fmt.Println("Hi")
+	_, to, dec, err := loader.Load("/home/kevin/Desktop/apt-pac/conv.csv")
 
-	from, to, dec, err := loader.Load("/home/kevin/Desktop/apt-pac/conv.csv")
-
-	fmt.Printf("from: %s\nto: %s\ndec: %+v\nerr: %v\n", from, to, dec, err)
-
-	args := []string{"upgrade"}
-	curr := dec
-	for _, arg := range args {
-		if curr == nil {
-			break
-		}
-		curr, _ = curr.Next(arg)
+	// fmt.Printf("from: %s\nto: %s\ndec: %+v\nerr: %v\n", from, to, dec, err)
+	if err != nil {
+		fmt.Println("load conv law failed!")
+		return
 	}
-	fmt.Printf("%s", *curr.Data)
-	return
 
-	cmd := exec.Command("pacman")
+	pattern, vars, ok := loader.Conv(dec, os.Args[1:])
+	if !ok {
+		fmt.Println("conv law failed!")
+		return
+	}
+	argses := loader.PatternToArgs(pattern, vars)
+	scanner := bufio.NewReader(os.Stdin)
+	for _, arg := range argses {
+		fmt.Printf("%+v\n", arg)
+		fmt.Print("Continue [y/n]?")
+		ans, _ := scanner.ReadString('\n')
+		if utils.Trim(ans) != "y" {
+			fmt.Println("Abort")
+			return
+		}
+		continue
 
-	cmd.Args = os.Args
+		cmd := exec.Command(to)
 
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
+		cmd.Args = arg
 
-	_ = cmd.Start()
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+
+		_ = cmd.Start()
+	}
 }
