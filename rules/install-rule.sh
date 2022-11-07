c6fd0f799814cd13db4a51dd2532a99e4d3d4cc5
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2022-11-07 01:26:40 +0000

    feat: install rule

---
+++ rules/install-rule.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+if_empty_exit () {
+  if [ -z "$1" ]
+  then
+    echo "Empty detected!"
+    exit 1
+  fi
+}
+
+ask_yes_or_exit () {
+    read -r -p "Continue? [Y/n] " ask_yes
+    case $ask_yes in
+        [yY][eE][sS]|[yY])
+            ;;
+        *)
+            echo "Abort..."
+            exit 1
+            ;;
+    esac
+}
+
+from=$1
+to=$2
+
+if_empty_exit "$from"
+echo "From: $from"
+if_empty_exit "$to"
+echo "To: $to"
+echo "So we are going to copy ./$from-$to.csv to /etc/konv/$from.csv"
+ask_yes_or_exit
+cp "./$from-$to.csv" "/etc/konv/$from.csv"
+
+echo "Make a link so that you can use the konv?"
+ask_yes_or_exit
+ln -s "/usr/bin/konv" "/usr/bin/$from"
